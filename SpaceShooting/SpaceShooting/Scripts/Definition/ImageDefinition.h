46763d73cde113489d0101ef2f0cf8561e9737a1
Takuto-Miyoshi <miyoshi.taku10@gmail.com>
2021-09-12 15:11:25 +0900

    ImageManagerの実装
    画像を管理する
    ・読み込み
    ・取得
    ・解放

---
+++ SpaceShooting/SpaceShooting/Scripts/Definition/ImageDefinition.h
@@ -0,0 +1,17 @@
+﻿// 画像に関する定義
+
+#ifndef IMAGE_DEFINITION_H
+#define IMAGE_DEFINITION_H
+
+#include <string>
+
+namespace shooting::image {
+    struct ImageData {
+        std::string filePath;  // 画像のパス
+        std::string name;  // 画像の登録名
+    };
+
+    const ImageData player { "Images/Object/Actor/Player.h", "Player" };
+}  // namespace shooting::image
+
+#endif  // !IMAGE_DEFINITION_H
---
+++ SpaceShooting/SpaceShooting/Scripts/Manager/ImageManager.h
@@ -0,0 +1,56 @@
+﻿// 画像を管理
+
+#ifndef IMAGE_MANAGER_H
+#define IMAGE_MANAGER_H
+
+#include <algorithm>
+#include <cstdint>
+#include <string>
+#include <vector>
+
+#include "../Definition/ImageDefinition.h"
+#include "../Utility/Singleton.h"
+#include "DxLib.h"
+
+namespace shooting {
+    class ImageManager : public Singleton<ImageManager> {
+       private:
+        struct RegistImageData {
+            int32_t graphicHandle;  // 画像のハンドル
+            std::string name;  // 登録名
+        };
+
+       public:
+        ImageManager() = default;
+
+        ~ImageManager() = default;
+
+       public:
+        /// @brief 読み込んだ画像を取得
+        /// @param name 登録名
+        auto Image( std::string name ) const -> int32_t {
+            auto result = std::find_if( imageDataList.begin(), imageDataList.end(), [&name]( auto element ) {
+                return element.name == name;
+            } );
+
+            // 最後の要素を警戒
+            return ( result->name != name ) ? -1 : result->graphicHandle;
+        }
+
+        /// @brief 画像の読み込み
+        void LoadGraphHandle( image::ImageData registData ) {
+            imageDataList.push_back( { LoadGraph( registData.filePath.c_str() ), registData.name } );
+        }
+
+        /// @brief 読み込んだ画像を全て解放
+        void Reset() {
+            InitGraph();
+            imageDataList = std::vector<RegistImageData>();
+        }
+
+       private:
+        std::vector<RegistImageData> imageDataList { std::vector<RegistImageData>() };
+    };
+}  // namespace shooting
+
+#endif  // !IMAGE_MANAGER_H
